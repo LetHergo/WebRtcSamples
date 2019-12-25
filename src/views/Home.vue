69dd0f8262aa407021ef1440754a3a8fb1991f1e
JiahongYi <895604998@qq.com>
2019-12-25 11:46:35 +0800

    初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,87 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="(item,index) in videoElement" :key="index" :ref="`divWrapper${index}`">
+      这是视频{{index+1}}
+      <video
+        autoplay
+        playsinline
+        :ref="`videoWrapper${index}`"
+        :srcObject="videoStream[index]"
+      ></video>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      videoElement: [],
+      videoStream: [],
+      videoSelect: '',
+      selectors: '',
+      constraints: {
+        video: true
+      }
+    }
+  },
+  mounted () {
+    this.init()
+    console.log(this.$refs['test1'])
+  },
+  computed: {},
+  methods: {
+    init () {
+      navigator.mediaDevices
+        .enumerateDevices()
+        .then(this.gotDevices)
+        .catch(this.handleError)
+
+      // if (window.stream) {
+      //   window.stream.getTracks().forEach(function (track) {
+      //     track.stop()
+      //   })
+      // }
+    },
+    handleError (error) {
+      console.log('Error: ', error)
+    },
+    async gotDevices (deviceInfos) {
+      // var constraints = new Array(deviceInfos.length)
+      for (var i = 0; i !== deviceInfos.length; ++i) {
+        var deviceInfo = deviceInfos[i]
+
+        if (deviceInfo.kind === 'videoinput') {
+          console.log(deviceInfo)
+          this.videoElement.push({
+            video: {
+              deviceId: deviceInfo.deviceId
+            },
+            audio: true
+          })
+          try {
+            const stream = await navigator.mediaDevices.getUserMedia(
+              this.videoElement[this.videoElement.length - 1]
+            )
+            this.gotStream(stream)
+          } catch (error) {
+            this.handleError(error)
+          }
+        }
+      }
+      // if (window.stream) {
+      //   window.stream.getTracks().forEach(function (track) {
+      //     track.stop()
+      //   })
+      // }
+    },
+    gotStream (stream) {
+      // window.stream = stream // make stream available to console
+      console.log(stream)
+      this.videoStream.push(stream)
+      this.$refs[`videoWrapper${this.videoStream.length - 1}`][0].srcObject = this.videoStream[this.videoStream.length - 1]
+    }
   }
 }
 </script>
